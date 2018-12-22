6a0a73260cbcb57f95bc0b40c4f63fbbaba40b0b
Alex Scotson <alexscotson@alexs-mbp.home>
2018-12-22 12:35:24 +0000

    added placeholders for login and register functionality

--- forum/resources/views/pages/index.blade.php
+++ forum/resources/views/pages/index.blade.php
@@ -6,6 +6,6 @@
         <p></p>
         <p>
             <a class="btn btn-primary btn-lg" href="/login" role="button"> Login </a>
-            <a class="btn btn-success btn-lg" href="register" role="button"> Register </a>
+            <a class="btn btn-success btn-lg" href="/register" role="button"> Register </a>
         </p>        
 @endsection
