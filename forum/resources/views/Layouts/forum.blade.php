9fa405e2339adf7a37447612c104801899a08d55
Alex Scotson <alexscotson@alexs-mbp.home>
2018-12-22 12:30:31 +0000

    added basic navbar functionality

--- forum/resources/views/Layouts/forum.blade.php
+++ forum/resources/views/Layouts/forum.blade.php
@@ -4,9 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}"> <!-- uses existing css file containing bootstap-->
-        <title>{{config('app.name', 'Games Forum')}}</title>
+        <title>{{config('app.name', 'ASForum')}}</title>
     </head>
     <body>
-        @yield('content')           <!--laravel blade snippets exention -->
+        @include('include.navbar')
+        <div class="container">
+            
+            @yield('content')           <!--laravel blade snippets exention -->
+        </div>
+
     </body>
 </html>
--- forum/resources/views/pages/index.blade.php
+++ forum/resources/views/pages/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.forum')
 
 @section('content')
-    <h1>Games Discussion Forum</h1>
-    <p></p>
+    <div class="jumbotron text-center">
+        <h1>Games Discussion Forum</h1>
+        <p></p>
+        <p>
+            <a class="btn btn-primary btn-lg" href="/login" role="button"> Login </a>
+            <a class="btn btn-success btn-lg" href="register" role="button"> Register </a>
+        </p>        
 @endsection
