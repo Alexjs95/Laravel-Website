cfed5a4ec52d7e48c6765ab0a4f9582666236695
Alex Scotson <alexscotson@Alexs-MacBook-Pro.local>
2018-12-28 11:46:08 +0000

    added post migration and set up env file to work with sql database

--- forum/resources/views/include/navbar.blade.php
+++ forum/resources/views/include/navbar.blade.php
@@ -15,7 +15,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="/assignment-1-laravel-Alexjs95/forum/public/services">Services</a>
             </li>
-        </ul>
+        </ul>  
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
