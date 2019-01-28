33bce43b943374ae702ac17eba04a9b87829bbb7
Alexjs95 <aj.scotson@hotmail.com>
2019-01-28 11:46:48 +0000

    added more text to about page

--- forum/resources/views/pages/about.blade.php
+++ forum/resources/views/pages/about.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
     <h1>About</h1>
-    <p>This website was created for an assignment using Laravel</p>
+    <p>This website was created for a University assignment using Laravele.</p><br>
+
+<p>The scenario was an online forum where authenticated users can carry out CRUD operations.
+    Discussion topics can be created which other users can view and respond. </p>
+
 @endsection
 
